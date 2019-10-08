7eb92384fe27101bc6673572bf14d5b733f3cc96
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-10-08 15:31:03 +0900

    05_05

--- main3.c
+++ main3.c
@@ -4,14 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=0,meter;
+	int i;
 	
-	while(i<3)
-	{
-		meter=i*1609;
-		printf("%d마일은 %d미터입니다.\n",i,meter);
-		i++;
-	}
-	
+	for(i=0;i<10;i++)
+	printf("교수님 너무 어려워요\n");
+
 	return 0;
 }
